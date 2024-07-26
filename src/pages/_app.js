c9ab751532b0fc5c5eb6e2072382d155349d31e5
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-26 07:40:44 +0000

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,14 @@ import '../css/main.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
+function MyApp({ Component, pageProps }) {
+    return (
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+        <SpeedInsights />
+      </>
+    );
+  }
+  
+  export default MyApp;
