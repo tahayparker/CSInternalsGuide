84563cd6f4a27343617990aef69a92222dbee026
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-26 10:55:11 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
                 '9/16': '56.25%'
             },
             fontFamily: {
-                fontPrimary: ['Saira', 'monospace'],
+                fontPrimary: ['Do', 'monospace'],
                 fontSecondary: ['Josefin Sans', 'monospace']
             },
             letterSpacing: {
